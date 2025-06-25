8645e2c1d2ad9447648d9a4a0492906589572095
khdegraffe <khdegraffe@gmail.com>
2025-06-24 20:41:02 -0400

    add drawCard function

--- deckOfCards.js
+++ deckOfCards.js
@@ -1,6 +1,25 @@
-const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
-const values = '1,2,3,4,5,6,7,8,9,10,J,Q,K,A';
-
-makeDeck() {
-  []
+function makeDeck() {
+  const deck = [];
 }
+
+function drawCard(deck) {
+  return deck.pop();
+}
+
+const myDeck = {
+  deck: [],
+  suits: ["hearts", "diamonds", "spades", "clubs"],
+  values: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+  initializeDeck() {
+    const { suits, values, deck } = this;
+    for (let value of values) {
+      for (let suit of suits) {
+        deck.push({ value, suit });
+      }
+    }
+    // return deck;
+  },
+  drawCard() {
+    return this.deck.pop();
+  },
+};
