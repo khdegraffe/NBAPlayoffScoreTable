3e6a0de63f6ae215a21000763a6ca1accb69e011
khdegraffe <khdegraffe@gmail.com>
2025-06-24 20:54:59 -0400

    add Fisher-Yates shuffle algo

--- deckOfCards.js
+++ deckOfCards.js
@@ -33,3 +33,14 @@ const myDeck = {
     return cards;
   },
 };
+
+//Fisher-Yates Shuffle algorithm
+function shuffle(arr) {
+  // loop over array backwards
+  for (let i = arr.length - 1; i > 0; i--) {
+    //pick random index before current element
+    let j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
